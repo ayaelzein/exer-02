655378b0e0e4a9b5589c8a947b0581eab53908c9
ayaelzein <aya.m.elzein@gmail.com>
2025-02-14 13:55:28 +0200

    first even and odd function

---
+++ ex-02.py
@@ -0,0 +1,9 @@
+print("Let's check if your number is even or odd")
+
+number = 0
+usinput = int(input("Please enter your number: "))
+
+if usinput == number + 1:
+    print("Your number is odd")
+elif usinput == number + 2:
+    print("Your number is even")
