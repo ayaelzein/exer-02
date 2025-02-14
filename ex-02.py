a8988f25c614677ba625fec70d6f9008d97b8d35
ayaelzein <aya.m.elzein@gmail.com>
2025-02-14 14:00:40 +0200

    resolved even or odd using comparsion funtion

--- ex-02.py
+++ ex-02.py
@@ -3,7 +3,7 @@ print("Let's check if your number is even or odd")
 number = 0
 usinput = int(input("Please enter your number: "))
 
-if usinput == number + 1:
-    print("Your number is odd")
-elif usinput == number + 2:
+if (usinput % 2) == number:
     print("Your number is even")
+else:
+    print("Your number is odd")
